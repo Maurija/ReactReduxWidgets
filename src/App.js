9a2a517eaf94be1dc41a23552a234baf91f6590f
Mauro <mauroevangelista08@gmail.com>
2022-11-15 11:09:58 -0300

    Dropdown finish

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
     {
@@ -17,11 +19,37 @@ const items = [
     }
 ];
 
+const options = [ 
+    {
+        label: 'The color red',
+        value: 'red'
+    },
+    {
+        label: 'The color green',
+        value: 'green'
+    },
+    {
+        label: 'The color blue',
+        value: 'blue'
+    }
+];
+
 const App = () =>{
+    const [selected,setSelected] = useState(options[0]);
+    const [showDropdown,setShowDropdown] = useState(true);
+
     return (
         <div>
             {/* <Accordion items={items}/> */}
-            <Search />
+            <button onClick={()=>setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
+            {showDropdown ?
+                <Dropdown                
+                selected={selected} 
+                onSelectedChange={setSelected}
+                options={options}/> :
+                null
+            }
+            
         </div>
 
     );
