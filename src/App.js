eeac627809e13ed5e73e5902d181bc6281e25791
Mauro <mauroevangelista08@gmail.com>
2022-11-15 13:37:31 -0300

    Test commit branch

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
 
 // APY KEY GOOGLE API : AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
-clase 188
+clase 188 -
 const items = [
     {
         title: "What is React?",
